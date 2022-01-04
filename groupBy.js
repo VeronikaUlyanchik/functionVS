4336a8437439f3ed3d648a31b618373371fcb1fe
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2022-01-04 22:37:58 +0300

    WebStorm

--- groupBy.js
+++ groupBy.js
@@ -13,14 +13,13 @@ function groupBy(arr,key){
     return newArr;}
 }
 
-const arr = [
-    { id: 1, universe: "marvel", name: "Spider Man" },
-    { id: 2, universe: "marvel", name: "Iron Man" },
-    { id: 3, universe: "dc", name: "Aqua Man" },
-    { id: 4, universe: "dc", name: "Bat Man" },
-    { id: 5, universe: "marvel", name: "Hulk" }
-    ]
+// const arr = [
+//     { id: 1, universe: "marvel", name: "Spider Man" },
+//     { id: 2, universe: "marvel", name: "Iron Man" },
+//     { id: 3, universe: "dc", name: "Aqua Man" },
+//     { id: 4, universe: "dc", name: "Bat Man" },
+//     { id: 5, universe: "marvel", name: "Hulk" }
+//     ]
 
-    console.log(groupBy(arr,'name'));
 
 
